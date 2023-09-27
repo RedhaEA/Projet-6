e98a7a17bc03be8722ff32d929cb83c7f3ca9c7b
RedhaEA <redhaelamrani@gmail.com>
2023-09-27 14:09:20 +0200

    fetch

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,15 @@
+
+
+
+
+
+
+
+
+
 const http = require('http');
 const app = require('./app');
+
 
 const normalizePort = val => {
 	const port = parseInt(val, 10);
--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -1,3 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-   
-  }, false);
+  const gallery = document.querySelector('.gallery');
+
+  // Utilisez fetch pour récupérer les travaux depuis l'API
+  fetch('http://localhost:5678/api/works', {
+    method: 'GET', // Utilisez la méthode GET pour récupérer les données
+    headers: {
+      'Content-Type': 'application/json', // Spécifiez le type de contenu JSON
+    },
+  })
+    .then(response => {
+
+      if (!response.ok) {
+        throw new Error('Erreur de réseau');
+      }
+      
+      return response.json().then(worksData => {
+        // Traitez les données récupérées et ajoutez-les à la galerie.
+        worksData.forEach(work => {
+          const workElement = document.createElement('div');
+          workElement.classList.add('work-item');
+          workElement.innerHTML = `
+          <img src="${work.imageUrl}" alt="${work.title}">
+          <h3>${work.title}</h3>
+          <p>${work.description}</p>
+        `;
+          gallery.appendChild(workElement);
+        });
+      })
+    })
+    .catch(error => {
+      console.error('Erreur :', error);
+    });
+}, false);
