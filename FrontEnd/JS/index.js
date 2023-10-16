d7955ae434ab8ba035bc871cfe6857fad7e622e5
Lowlighto <jad.elamrani.fr@gmail.com>
2023-10-16 19:25:37 +0200

    Button modifier visible que si connecté

--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -1,42 +1,43 @@
 document.addEventListener(
   'DOMContentLoaded',
   function () {
-    const filters = document.querySelectorAll('.filters button')
-    const buttonProjects = document.querySelectorAll(".open-modal");
-    
+    // Tout le code ici s'execute une fois le document chargé
+    fetchWorks();
 
-    // Utilisez fetch pour récupérer les travaux depuis l'API
-    fetch('http://localhost:5678/api/works', {
-      method: 'GET', // Utilisez la méthode GET pour récupérer les données
-      headers: {
-        'Content-Type': 'application/json' // Spécifiez le type de contenu JSON
-      }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Erreur de réseau')
-        }
-        return response.json().then(worksData => {
-          let filteredWorks = worksData
-          filters.forEach(filterButton => {
-            filterButton.addEventListener('click', () => {
-              filteredWorks = worksData.filter(
-                work =>
-                  work.category.name === filterButton.textContent ||
-                  filterButton.textContent === 'Tous'
-              )
-              displayWorks(filteredWorks)
-            })
-          })
-          displayWorks(filteredWorks)
-        })
-      })
-      .catch(error => {
-        console.error('Erreur :', error)
-      })
+    const loginBtn = document.getElementById("login");
+    const disconnectBtn = document.getElementById("disconnect");
+
+    disconnectBtn.addEventListener("click", () => {
+      localStorage.clear();
+      this.location.reload();
+    });
+
+    if (isConnected()) {
+      loginBtn.classList.add("display-none");
+      disconnectBtn.classList.remove("display-none");
+    } else {
+      loginBtn.classList.remove("display-none");
+      disconnectBtn.classList.add("display-none");
+    }
+
+    const modifyButton = document.getElementById("modify-projects");
+    modifyButton.addEventListener("click", () => alert("click_ok"));
+
+    if (isConnected())
+      modifyButton.classList.remove("display-none");
   },
   false
 )
+
+function isConnected() {
+  var token = localStorage.getItem("token");
+
+  if (token != null) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 const displayWorks = works => {
   const gallery = document.querySelector('.gallery')
@@ -64,10 +65,37 @@ function closeModal() {
   modal.style.display = "none";
 }
 
-// Lorsqu'un bouton est cliqué, ouvrir la modale
-const openButton = document.querySelector(".open-modal");
-openButton.addEventListener("click", openModal);
+function fetchWorks() {
+  const filters = document.querySelectorAll('.filters button');
 
-// Lorsqu'un bouton de fermeture est cliqué, fermer la modale
-const closeButton = document.querySelector(".closeModale");
-closeButton.addEventListener("click", closeModal);
+  // Utilisez fetch pour récupérer les travaux depuis l'API
+  fetch('http://localhost:5678/api/works', {
+    method: 'GET', // Utilisez la méthode GET pour récupérer les données
+    headers: {
+      'Content-Type': 'application/json' // Spécifiez le type de contenu JSON
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Erreur de réseau')
+      }
+      return response.json().then(worksData => {
+        let filteredWorks = worksData
+        filters.forEach(filterButton => {
+          filterButton.addEventListener('click', () => {
+            filteredWorks = worksData.filter(
+              work =>
+                work.category.name === filterButton.textContent ||
+                filterButton.textContent === 'Tous'
+            )
+            displayWorks(filteredWorks)
+          })
+        })
+        displayWorks(filteredWorks)
+      })
+    })
+    .catch(error => {
+      console.error('Erreur :', error)
+    });
+}
+
